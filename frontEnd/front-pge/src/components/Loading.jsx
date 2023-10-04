a5b742745135582d84ef9ca88f58ac72c83b9d2a
Uneto47 <netovasconcelos02@gmail.com>
2023-10-04 00:07:24 -0300

    feat: Add loading component

---
+++ frontEnd/front-pge/src/components/Loading.jsx
@@ -0,0 +1,8 @@
+function Loading() {
+  return <div className="flex justify-center items-center h-[100vh]">
+    <span className="loading loading-spinner loading-lg text-[#1E3A8A]"></span>
+  </div>
+}
+
+export default Loading;
+
