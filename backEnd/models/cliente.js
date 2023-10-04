9199ca832c789880fb324a73d90efbd79b8e9ef9
Uneto47 <netovasconcelos02@gmail.com>
2023-10-04 16:53:17 -0300

    feat: add password

--- backEnd/models/cliente.js
+++ backEnd/models/cliente.js
@@ -4,7 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 const clienteSchema = new mongoose.Schema({
   _id: {type: String, default: uuidv4()},
   nome: { type: String, required: true },
-  cpf: { type: String, required: true, unique: true }
+  cpf: { type: String, required: true, unique: true },
+  senha: { type: String, required: true, select: false}
 });
 
 const Cliente = mongoose.model('Cliente', clienteSchema);
