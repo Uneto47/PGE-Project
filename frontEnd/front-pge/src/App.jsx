560a99ff610f5ed57393060fbc63fef830dfd315
Uneto47 <netovasconcelos02@gmail.com>
2023-10-04 00:06:15 -0300

    chore: Update app file

--- frontEnd/front-pge/src/App.jsx
+++ frontEnd/front-pge/src/App.jsx
@@ -1,8 +1,33 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import { pages } from "./data/pages";
+import { lazy, Suspense, useEffect } from "react";
+import Loading from "./components/Loading";
+
+const routes = pages.map(e => {
+  const Page = lazy(() => import(`./Pages/${e.filename}.jsx`));
+  return <Route key={e.name} path={e.route} element={<Page />} />;
+});
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    let agent = navigator.userAgent;
+    if (agent.match(/firefox|fxios/i)) {
+      window.scrollTo(0, 0);
+    }
+    else {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }
+  }, [location]);
 
   return (
     <div>
-      <p className='flex flex-col items-center text-3xl'>Hello</p>
+      <Suspense fallback={<Loading/>}>
+        <Routes>
+          {routes}
+        </Routes>
+      </Suspense>
     </div>
   )
 }
