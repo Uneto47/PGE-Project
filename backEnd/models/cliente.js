6ee6bc131c98fa665f3ab82dba4d0c9c0ec12b50
Uneto47 <netovasconcelos02@gmail.com>
2023-10-04 20:56:10 -0300

    fix: update id automatically

--- backEnd/models/cliente.js
+++ backEnd/models/cliente.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const clienteSchema = new mongoose.Schema({
-  _id: {type: String, default: uuidv4()},
+  _id: { type: String },
   nome: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
-  senha: { type: String, required: true, select: false}
+  senha: { type: String, required: true, select: false }
+});
+
+clienteSchema.pre('save', function (next) {
+  if (!this._id) {
+    this._id = uuidv4();
+  }
+  next();
 });
 
 const Cliente = mongoose.model('Cliente', clienteSchema);
