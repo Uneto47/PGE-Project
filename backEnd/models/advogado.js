f140f8f5c662fb02c136b5ba47d9a7d26786cf7b
Uneto47 <netovasconcelos02@gmail.com>
2023-10-04 16:52:44 -0300

    feat: add password and oab is required

--- backEnd/models/advogado.js
+++ backEnd/models/advogado.js
@@ -5,7 +5,8 @@ const AdvogadosSchema = new mongoose.Schema({
   _id: {type: String, default: uuidv4()},
   nome: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
-  oab: { type:String }
+  oab: { type:String, require: true },
+  senha: {type: String, require: true, select: false}
 });
 
 const Advogados = mongoose.model('Advogados', AdvogadosSchema);
