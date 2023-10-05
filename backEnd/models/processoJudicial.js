fbfc8619bf84f9678fb4171158dc5b085004059d
Uneto47 <netovasconcelos02@gmail.com>
2023-10-05 13:54:31 -0300

    fix: type's table processoJudicialSchema

--- backEnd/models/processoJudicial.js
+++ backEnd/models/processoJudicial.js
@@ -4,9 +4,9 @@ const { v4: uuidv4 } = require('uuid');
 const processoJudicialSchema = new mongoose.Schema({
   _id: {type: String, default: uuidv4()},
   numeroprocesso: { type: String, required: true, unique: true },
-  parte: { type: mongoose.Schema.Types.ObjectId, ref: 'ClienteAdvogado' },
-  responsavel: { type: mongoose.Schema.Types.ObjectId, ref: 'ClienteAdvogado' },
-  documentos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Documento'}],
+  parte: { type: String, ref: 'Cliente' },
+  responsavel: { type: String, ref: 'Advogado' },
+  documentos: [{ type: String, ref: 'Documento'}],
   tema: String,
   valorcausa: Number,
 });
