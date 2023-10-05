d2f216649ac5f3a3e8129b8e4f5d447d563db468
Uneto47 <netovasconcelos02@gmail.com>
2023-10-05 13:52:37 -0300

    feat: add lawyers authentication

--- backEnd/routes/AuthRoutes.js
+++ backEnd/routes/AuthRoutes.js
@@ -3,28 +3,46 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Cliente = require('../models/cliente');
+const Advogado = require('../models/advogado');
 
-router.post('/login', async (req, res) => {
+const  fazerAutenticacao = async ( usuario, senha, res) => {
+  const senhaCorrespondente = await bcrypt.compare(senha, usuario.senha);
+
+  if (!senhaCorrespondente) return res.status(404).json({ error: 'Usuário ou Senha inválido' })
+  
+  const { _id } = usuario
+  const token = jwt.sign({ _id }, process.env.DB_PASSWORD, { expiresIn: "1 day" })
+  res.send({ auth: true, token: token })
+
+}
+
+router.post('/login/clientes', async (req, res) => {
   const { cpf, senha } = req.body
   try {
     const cliente = await Cliente.findOne({ cpf }).select('+senha')
+    
+    if (!cliente) return res.status(404).json({ error: 'Usuário ou Senha inválido' })
 
-    if (!cliente) {
-      return res.status(401).json({ error: 'Usuário não encontrado' });
-    }
-
-    const senhaCorrespondente = await bcrypt.compare(senha, cliente.senha);
-
-    if (!senhaCorrespondente) {
-      return res.status(401).json({ error: 'Senha inválida' });
-    }
-
-    const { _id } = cliente
-    const token = jwt.sign({ _id }, process.env.DB_PASSWORD, { expiresIn: "1 day" })
-    res.send({ auth: true, token: token })
-
+    await fazerAutenticacao(cliente, senha, res)
+   
   } catch (err) {
-    return res.status(401).send(err.message)
+    console.error(err)
+    return res.status(500).send(err.message)
+  }
+});
+
+router.post('/login/advogados', async (req, res) => {
+  const { cpf, senha } = req.body
+  try {
+    const advogado = await Advogado.findOne({ cpf }).select('+senha')
+    
+    if (!advogado) return res.status(404).json({ error: 'Usuário ou Senha inválido' });
+
+   await fazerAutenticacao(advogado, senha, res)
+      
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send(err.message)
   }
 });
 
