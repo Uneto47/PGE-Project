7ec7d8dc68932d2264424d537a5ee8c0568a65c2
Uneto47 <netovasconcelos02@gmail.com>
2023-10-06 18:42:35 -0300

    feat: add id function

--- backEnd/models/processoJudicial.js
+++ backEnd/models/processoJudicial.js
@@ -2,13 +2,20 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const processoJudicialSchema = new mongoose.Schema({
-  _id: {type: String, default: uuidv4()},
+  _id: { type: String },
   numeroprocesso: { type: String, required: true, unique: true },
   parte: { type: String, ref: 'Cliente' },
   responsavel: { type: String, ref: 'Advogado' },
   documentos: [{ type: String, ref: 'Documento'}],
   tema: String,
   valorcausa: Number,
+});
+
+processoJudicialSchema.pre('save', function (next) {
+  if (!this._id) {
+    this._id = uuidv4();
+  }
+  next();
 });
 
 const ProcessoJudicial = mongoose.model('ProcessoJudicial', processoJudicialSchema);
