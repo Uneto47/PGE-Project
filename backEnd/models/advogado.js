dedb8b2d2c970b2e6198bcc763a4d3ba74e8d55c
Uneto47 <netovasconcelos02@gmail.com>
2023-10-05 23:37:18 -0300

    feat: add id information in advogado file

--- backEnd/models/advogado.js
+++ backEnd/models/advogado.js
@@ -2,11 +2,18 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const AdvogadosSchema = new mongoose.Schema({
-  _id: {type: String, default: uuidv4()},
+  _id: { type: String },
   nome: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
   oab: { type:String, require: true },
   senha: {type: String, require: true, select: false}
+});
+
+AdvogadosSchema.pre('save', function (next) {
+  if (!this._id) {
+    this._id = uuidv4();
+  }
+  next();
 });
 
 const Advogados = mongoose.model('Advogados', AdvogadosSchema);
