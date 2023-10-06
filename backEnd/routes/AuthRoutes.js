ca43bf2b221835164e5b37ea50ba1290579efd9c
Uneto47 <netovasconcelos02@gmail.com>
2023-10-06 11:24:52 -0300

    feat: add new flag type

--- backEnd/routes/AuthRoutes.js
+++ backEnd/routes/AuthRoutes.js
@@ -5,14 +5,14 @@ const jwt = require('jsonwebtoken');
 const Cliente = require('../models/cliente');
 const Advogado = require('../models/advogado');
 
-const  fazerAutenticacao = async ( usuario, senha, res) => {
+const fazerAutenticacao = async (usuario, senha, res, tipo) => {
   const senhaCorrespondente = await bcrypt.compare(senha, usuario.senha);
 
   if (!senhaCorrespondente) return res.status(404).json({ error: 'Usuário ou Senha inválido' })
-  
+
   const { _id } = usuario
-  const token = jwt.sign({ _id }, process.env.DB_PASSWORD, { expiresIn: "1 day" })
-  res.send({ auth: true, id:_id , token: token })
+  const token = jwt.sign({ _id, tipo: tipo }, process.env.DB_PASSWORD, { expiresIn: "1 day" })
+  res.send({ auth: true, id: _id, token: token, tipo: tipo })
 
 }
 
@@ -20,11 +20,12 @@ router.post('/login/clientes', async (req, res) => {
   const { cpf, senha } = req.body
   try {
     const cliente = await Cliente.findOne({ cpf }).select('+senha')
-    
+
     if (!cliente) return res.status(404).json({ error: 'Usuário ou Senha inválido' })
 
-    await fazerAutenticacao(cliente, senha, res)
-   
+    const tipo = "cliente"
+    await fazerAutenticacao(cliente, senha, res, tipo)
+
   } catch (err) {
     console.error(err)
     return res.status(500).send(err.message)
@@ -35,11 +36,12 @@ router.post('/login/advogados', async (req, res) => {
   const { cpf, senha } = req.body
   try {
     const advogado = await Advogado.findOne({ cpf }).select('+senha')
-    
+
     if (!advogado) return res.status(404).json({ error: 'Usuário ou Senha inválido' });
 
-   await fazerAutenticacao(advogado, senha, res)
-      
+    const tipo = "advogado"
+    await fazerAutenticacao(advogado, senha, res, tipo)
+
   } catch (err) {
     console.error(err)
     return res.status(500).send(err.message)
