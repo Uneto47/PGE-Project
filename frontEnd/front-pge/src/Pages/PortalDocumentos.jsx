6a824d9cf64f29dd76da619385bc4c81e15c9351
Uneto47 <netovasconcelos02@gmail.com>
2023-10-06 11:22:01 -0300

    feat: add type user for loading data

--- frontEnd/front-pge/src/Pages/PortalDocumentos.jsx
+++ frontEnd/front-pge/src/Pages/PortalDocumentos.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import Documentos from "../components/documentos/Documentos";
 
 const BASE_URL = "http://localhost:3000";
@@ -8,32 +8,52 @@ const BASE_URL = "http://localhost:3000";
 function PortalDocumentos() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
-  const [clienteData, setClienteData] = useState(null);
+  const [usuarioData, setUsuarioData] = useState(null);
   const [processoData, setProcessoData] = useState(null);
-  const [advogadoData, setAdvogadoData] = useState(null);
+  const [envolvido, setEnvolvido] = useState(null);
+  const location = useLocation();
+  const tipoRecebido = location.state.tipo;
+
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const clienteResponse = await axios.get(`${BASE_URL}/clientes/${id}`);
-        setClienteData(clienteResponse.data);
-        console.log(clienteResponse.data);
 
-        const processoResponse = await axios.get(`${BASE_URL}/processos-judiciais/processo/${id}`);
-        setProcessoData(processoResponse.data);
-        console.log(processoResponse.data);
+      if (tipoRecebido === "cliente") {
+        try {
+          const usuarioResponse = await axios.get(`${BASE_URL}/clientes/${id}`);
+          setUsuarioData(usuarioResponse.data);
 
-        const advogadoResponse = await axios.get(`${BASE_URL}/advogados/${processoResponse.data.responsavel}`);
-        setAdvogadoData(advogadoResponse.data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
+          const processoResponse = await axios.get(`${BASE_URL}/processos-judiciais/processo/cliente/${id}`);
+          setProcessoData(processoResponse.data);
+
+          const envolvidoResponse = await axios.get(`${BASE_URL}/advogados/${processoResponse.data.responsavel}`);
+          setEnvolvido(envolvidoResponse.data);
+
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      } else if (tipoRecebido === "advogado") {
+        try {
+          const databaseResponse = await axios.get(`${BASE_URL}/advogados/${id}`);
+          setUsuarioData(databaseResponse.data);
+
+          const processoResponse = await axios.get(`${BASE_URL}/processos-judiciais/processo/advogado/${id}`);
+          setProcessoData(processoResponse.data);
+
+          const envolvidoResponse = await axios.get(`${BASE_URL}/clientes/${processoResponse.data.parte}`);
+          setEnvolvido(envolvidoResponse.data);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
       }
     };
 
     fetchData();
-  }, [id]);
+  }, []);
 
   return (
     <div>
@@ -41,14 +61,14 @@ function PortalDocumentos() {
         <p>Carregando dados...</p>
       ) : (
         <div>
-          <h1>Dados do cliente:</h1>
-          <p>Nome: {clienteData.nome}</p>
-          <p>CPF: {clienteData.cpf}</p>
+          <h1>Dados do {tipoRecebido}:</h1>
+          <p>Nome: {usuarioData.nome}</p>
+          <p>CPF: {usuarioData.cpf}</p>
           {processoData ? (
             <div>
               <h1>Dados do processo judicial:</h1>
               <p>Número: {processoData.numeroprocesso}</p>
-              <p>Responsável: {advogadoData.nome}</p>
+              <p>{tipoRecebido === "advogado" ? "Cliente" : "Advogado"}: {envolvido.nome}</p>
               <Documentos processoData={processoData} />
               <p>Tema: {processoData.tema}</p>
               <p>Valor da Causa: {processoData.valorcausa}</p>
