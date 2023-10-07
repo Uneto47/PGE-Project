121c12cd6242a70443e950104f91e280782fd61f
Uneto47 <netovasconcelos02@gmail.com>
2023-10-07 14:17:28 -0300

    feat: add id

--- backEnd/models/documentos.js
+++ backEnd/models/documentos.js
@@ -2,10 +2,17 @@ const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
 const documentoSchema = new mongoose.Schema({
-  _id: {type: String, default: uuidv4()},
-  nome: { type: String, required: true },
+  _id: { type: String },
+  nome: { type: String, required: true, unique: true },
   caminho: { type: String, required: true },
   extensao: { type: String, required: true },
+});
+
+documentoSchema.pre('save', function (next) {
+  if (!this._id) {
+    this._id = uuidv4();
+  }
+  next();
 });
 
 const Documento = mongoose.model('Documento', documentoSchema);
